55ceddd760f8fac52f56792d022dcb1b7342f4a9
kenctrl <kennethkchoi22@gmail.com>
2024-12-06 18:57:11 -0500

    add back in pip install

--- final_project_devctr-main/run-template.sh
+++ final_project_devctr-main/run-template.sh
@@ -3,6 +3,10 @@
 echo "Content:"; ls
 python3 exp/run_join_exp.py -s /tmp/join_exp_config.csv
 
+# Install the required packages (once a day)
+# echo "Installing the required packages"
+# python3 -m pip install -r requirements.txt
+
 echo "Run microbenchmarks from Section 5.2.1 to 5.2.7"
 python3 exp/run_join_exp.py \
         -b ./bin/volcano/join_exp_4b4b \
