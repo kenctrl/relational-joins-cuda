8a251bc566c83b14deb4aea6de80da772658fdf8
Siddhant Mukherjee <sidm1811@users.noreply.github.com>
2024-12-08 16:15:06 -0500

    Coop thread groups buggy

--- final_project_devctr-main/src/src/volcano/partition.cu
+++ final_project_devctr-main/src/src/volcano/partition.cu
@@ -45,6 +45,150 @@ __device__ BucketT get_bucket(KeyT key) {
     return key >> low_bit & ((1 << (high_bit - low_bit)) - 1);
 }
 
+// // CountT must support atomic writes
+// template<
+//     typename KeyT,
+//     typename ValueT,
+//     typename CountT,
+//     typename BucketT,
+//     int NUM_THREADS, 
+//     int THREAD_TILE, 
+//     int low_bit, 
+//     int high_bit>
+// __global__ void partition_old(KeyT* key_array, int size, void* workspace, void* out, void* debug_out) {
+//     CountT* debug_ptr = (CountT*) debug_out;
+
+//     assert(gridDim.y == 1 && gridDim.z == 1); // We only support 1D grids for now
+//     assert(blockDim.y == 1 && blockDim.z == 1); // We only support 1D blocks for now
+//     assert(blockDim.x == NUM_THREADS); // We only support 1D blocks for now
+
+//     auto BLOCK_TILE = NUM_THREADS * THREAD_TILE;
+//     auto NUM_BUCKETS = 1 << (high_bit - low_bit);
+
+//     // Block space: We put status flags in top two bits
+//     // Block space points to current block's histogram in workspace
+//     CountT* block_space = static_cast<CountT*>(workspace) + blockIdx.x * NUM_BUCKETS;
+//     // Global scan array starts after all block histograms
+//     CountT* global_block_scan = block_space + NUM_BUCKETS * gridDim.x;
+//     KeyT* output = (KeyT*) out;
+
+//     // Assert that global_block_scan and output don't overlap
+//     assert((global_block_scan + NUM_BUCKETS) <= output || 
+//            (output + size) <= global_block_scan);
+
+//     CountT PREFIX_DONE = CountT(1) << (sizeof(CountT) * 8 - 1); // Top bit set means prefix sum is done
+//     CountT AGG_DONE = CountT(1) << (sizeof(CountT) * 8 - 2); // Second top bit set means aggregation is done
+//     CountT ALL_DONE = PREFIX_DONE | AGG_DONE;
+
+//     assert(__popc(PREFIX_DONE) == 1);
+//     assert(__popc(AGG_DONE) == 1);
+//     assert(__popc(ALL_DONE) == 2);
+
+//     // Shared memory
+//     extern __shared__ CountT local_hist[];
+
+//     // // Initialize local histogram
+//     for (auto i = threadIdx.x; i < NUM_BUCKETS; i += NUM_THREADS) {
+//         local_hist[i] = 0;
+//     }
+//     __syncthreads();
+
+//     // Compute local histogram
+//     for (auto i = blockIdx.x * BLOCK_TILE + threadIdx.x; i < (blockIdx.x + 1) * BLOCK_TILE && i < size; i += NUM_THREADS) {
+//         BucketT key = get_bucket<BucketT, KeyT, low_bit, high_bit>(key_array[i]);
+//         // // uncoalesced writes: Optimize this later with warp shuffle
+//         atomicAdd(&local_hist[key], 1);
+//     };
+//     __syncthreads();
+
+//     CountT write_status = (blockIdx.x == 0)? PREFIX_DONE: AGG_DONE;
+
+//     for (auto i = threadIdx.x; i < NUM_BUCKETS; i += NUM_THREADS) {
+//         // We should not have any status flags set in local_hist
+//         assert((local_hist[i] & (PREFIX_DONE | AGG_DONE)) == 0);
+//         auto write_data = write_status | local_hist[i];
+//         // Assume writes are atomic - value and status are written together
+//         assert(&block_space[i] < global_block_scan);
+//         block_space[i] = write_data;
+//     }
+
+//     // Each thread is responsible for some number of buckets
+//     // It performs a decoupled lookback to get the prefix sum for its buckets
+//     for (auto cur_bucket = threadIdx.x; cur_bucket < NUM_BUCKETS; cur_bucket += NUM_THREADS) {
+//         int prev_block = (int) blockIdx.x - 1;
+//         CountT local_val = local_hist[cur_bucket]; // No status flags set
+//         while (prev_block >= 0) {
+//             auto prev_space = ((CountT*) workspace) + prev_block * NUM_BUCKETS;
+//             auto prev_status = prev_space[cur_bucket];
+//             if (prev_status & PREFIX_DONE) {
+//                 local_val += prev_status & ~(PREFIX_DONE | AGG_DONE); // Clear status flags
+//                 break;
+//             } else if (prev_status & AGG_DONE) {
+//                 local_val += prev_status & ~(PREFIX_DONE | AGG_DONE); // Clear status flags
+//                 prev_block--;
+//             } else {
+//                 // Keep spinning
+//             }
+//         }
+//         // We should not have any status flags set in local_val
+//         assert((local_val & (PREFIX_DONE | AGG_DONE)) == 0);
+//         // No race here - write to global
+//         // Set status flag to PREFIX_DONE
+//         assert(&block_space[cur_bucket] < global_block_scan);
+//         block_space[cur_bucket] = local_val | PREFIX_DONE;
+//     }
+//     if (blockIdx.x == gridDim.x - 1) {
+//         // Simple code for now - we know that thread x has the prefix sum for its buckets
+//         for (auto cur_bucket = threadIdx.x; cur_bucket < NUM_BUCKETS; cur_bucket += NUM_THREADS) {
+//             auto thread_val = block_space[cur_bucket] & ~(PREFIX_DONE | AGG_DONE);
+//             for (auto j = cur_bucket; j < NUM_BUCKETS; j++) {
+//                 atomicAdd(&global_block_scan[j], thread_val);
+//                 // Other threads should be blocked
+//                 assert((global_block_scan[j] & ALL_DONE) == 0);
+//             }
+//         }
+//         __syncthreads();
+//         for (auto cur_bucket = threadIdx.x; cur_bucket < NUM_BUCKETS; cur_bucket += NUM_THREADS) {
+//             // Atomic OR
+//             global_block_scan[cur_bucket] |= ALL_DONE;
+//             // Unblock other threads
+//             assert((global_block_scan[cur_bucket] & ALL_DONE) == ALL_DONE);
+//         }
+//     }
+//     // cooperative_groups::thread_group g = cooperative_groups::this_grid();
+//     // g.sync();
+//     // // Implicit groups?
+
+//     // // Now we have the global prefix sum
+//     // Now write out the data - each thread is responsible for a tile
+//     for (auto i = blockIdx.x * BLOCK_TILE; i < (blockIdx.x + 1) * BLOCK_TILE && i < size; i++) {
+//         BucketT bucket = get_bucket<BucketT, KeyT, low_bit, high_bit>(key_array[i]);
+//         // Check if bucket belongs to this thread
+//         if (bucket % blockDim.x != threadIdx.x) {
+//             continue;
+//         }
+//         // Bucket internal offset, remove status flags
+//         auto local_offset = (--block_space[bucket]) & ~(PREFIX_DONE | AGG_DONE);
+//         // Offset from previous buckets - inclusive scan
+//         auto global_offset = bucket > 0? global_block_scan[bucket - 1]: ALL_DONE;
+//         // if (bucket == 0) assert((global_offset & ALL_DONE) == ALL_DONE);
+//         while((global_offset & ALL_DONE) != ALL_DONE) {
+//             // Spin until unblocked by global scan
+//             // assert(bucket > 0);
+//             global_offset = global_block_scan[bucket - 1];
+//         }
+//         assert((global_offset & ALL_DONE) == ALL_DONE);
+//         // assert(bucket == 0 || (global_block_scan[bucket-1] & ALL_DONE) == ALL_DONE);
+//         // assert(bucket == 0 || global_offset == global_block_scan[bucket - 1]);
+//         global_offset &= ~ALL_DONE;
+//         // Clear status flags
+//         auto out_index = global_offset + local_offset;
+//         // No race - each thread updates its own buckets
+//         output[out_index] = key_array[i]; // Reverse order!!
+//         assert(out_index < size && out_index >= 0);
+//     }
+// }
+
 // CountT must support atomic writes
 template<
     typename KeyT,
@@ -119,12 +263,12 @@ __global__ void partition(KeyT* key_array, int size, void* workspace, void* out,
         CountT local_val = local_hist[cur_bucket]; // No status flags set
         while (prev_block >= 0) {
             auto prev_space = ((CountT*) workspace) + prev_block * NUM_BUCKETS;
-            auto prev_status = prev_space[cur_bucket];
+            auto prev_status = prev_space[cur_bucket] & ~(PREFIX_DONE | AGG_DONE);
             if (prev_status & PREFIX_DONE) {
-                local_val += prev_status & ~(PREFIX_DONE | AGG_DONE); // Clear status flags
+                local_val += prev_status; // Clear status flags
                 break;
             } else if (prev_status & AGG_DONE) {
-                local_val += prev_status & ~(PREFIX_DONE | AGG_DONE); // Clear status flags
+                local_val += prev_status; // Clear status flags
                 prev_block--;
             } else {
                 // Keep spinning
@@ -147,16 +291,9 @@ __global__ void partition(KeyT* key_array, int size, void* workspace, void* out,
                 assert((global_block_scan[j] & ALL_DONE) == 0);
             }
         }
-        __syncthreads();
-        for (auto cur_bucket = threadIdx.x; cur_bucket < NUM_BUCKETS; cur_bucket += NUM_THREADS) {
-            // Atomic OR
-            global_block_scan[cur_bucket] |= ALL_DONE;
-            // Unblock other threads
-            assert((global_block_scan[cur_bucket] & ALL_DONE) == ALL_DONE);
-        }
     }
-    // cooperative_groups::thread_group g = cooperative_groups::this_grid();
-    // g.sync();
+    cooperative_groups::thread_group g = cooperative_groups::this_grid();
+    g.sync();
     // // Implicit groups?
 
     // // Now we have the global prefix sum
@@ -170,29 +307,24 @@ __global__ void partition(KeyT* key_array, int size, void* workspace, void* out,
         // Bucket internal offset, remove status flags
         auto local_offset = (--block_space[bucket]) & ~(PREFIX_DONE | AGG_DONE);
         // Offset from previous buckets - inclusive scan
-        auto global_offset = bucket > 0? global_block_scan[bucket - 1]: ALL_DONE;
-        if (bucket == 0) assert((global_offset & ALL_DONE) == ALL_DONE);
-        while((global_offset & ALL_DONE) != ALL_DONE) {
-            // Spin until unblocked by global scan
-            global_offset = global_block_scan[bucket - 1];
-        }
-        assert((global_offset & ALL_DONE) == ALL_DONE);
-        // assert(bucket == 0 || (global_block_scan[bucket-1] & ALL_DONE) == ALL_DONE);
-        // assert(bucket == 0 || global_offset == global_block_scan[bucket - 1]);
-        global_offset &= ~ALL_DONE;
+        auto global_offset = bucket > 0? global_block_scan[bucket - 1]: 0;
         // Clear status flags
         auto out_index = global_offset + local_offset;
         // No race - each thread updates its own buckets
-        output[out_index] = key_array[i]; // Reverse order!!
         assert(out_index < size && out_index >= 0);
+        output[out_index] = key_array[i]; // Reverse order!!
     }
 }
+
 
 void test_1(){
     using KeyT = uint32_t;
     using ValueT = uint32_t;
     using CountT = uint32_t;
     using BucketT = uint32_t;
+
+    constexpr int NUM_THREADS = 32;
+    constexpr int THREAD_TILE = 1;
 
     auto NUM_BUCKETS = 1 << (HIGH_BIT - LOW_BIT);
 
@@ -207,8 +339,8 @@ void test_1(){
     cudaMalloc(&key_array, size * sizeof(uint32_t));
     cudaMemcpy(key_array, host_array, size * sizeof(uint32_t), cudaMemcpyHostToDevice);
 
-    dim3 grid_dim((size + 31) / 32);
-    dim3 block_dim(32);
+    dim3 grid_dim((size + NUM_THREADS * THREAD_TILE - 1) / (NUM_THREADS * THREAD_TILE));
+    dim3 block_dim(NUM_THREADS);
 
     auto workspace_size = get_workspace_size<CountT, LOW_BIT, HIGH_BIT>(grid_dim, block_dim);
     auto shmem_size = get_shmem_size<CountT, LOW_BIT, HIGH_BIT>(grid_dim, block_dim);
@@ -228,21 +360,37 @@ void test_1(){
     cudaMemset(debug_out, 0, NUM_BUCKETS * sizeof(uint32_t));
     CUDA_CHECK(cudaDeviceSynchronize());
 
-    // void* args[] = {
-    //     (void*)key_array,
-    //     (void*)&size,
-    //     (void*)workspace,
-    //     (void*)out,
-    //     (void*)debug_out
-    // };
-    // cudaLaunchCooperativeKernel(
-    //     reinterpret_cast<void*>(partition<KeyT, ValueT, CountT, BucketT, 32, 1, LOW_BIT, HIGH_BIT>),
-    //     grid_dim,
-    //     block_dim,
-    //     args,
-    //     shmem_size
-    // );
-    partition<KeyT, ValueT, CountT, BucketT, 32, 1, LOW_BIT, HIGH_BIT><<<grid_dim, block_dim, shmem_size>>>(key_array, size, workspace, out, debug_out);
+    cudaDeviceProp deviceProp;
+    int dev = 0;
+    int numBlocksPerSm = 0;
+    cudaGetDeviceProperties(&deviceProp, dev);
+    cudaOccupancyMaxActiveBlocksPerMultiprocessor(&numBlocksPerSm, 
+        partition<KeyT, ValueT, CountT, BucketT, NUM_THREADS, THREAD_TILE, LOW_BIT, HIGH_BIT>, 
+        NUM_THREADS, shmem_size);
+
+    dim3 dimBlock(NUM_THREADS, 1, 1);
+    dim3 dimGrid(deviceProp.multiProcessorCount*numBlocksPerSm, 1, 1);
+
+    std::cout << "Blocks per SM: " << numBlocksPerSm << "\n";
+    std::cout << "Blocks: " << dimGrid.x << "\n";
+
+    assert(dimGrid.x >= grid_dim.x);
+
+    void* args[] = {
+        (void*)&key_array,
+        (void*)&size,
+        (void*)&workspace,
+        (void*)&out,
+        (void*)&debug_out
+    };
+    cudaLaunchCooperativeKernel(
+        reinterpret_cast<void*>(partition<KeyT, ValueT, CountT, BucketT, 32, 1, LOW_BIT, HIGH_BIT>),
+        dimGrid,
+        dimBlock,
+        args,
+        shmem_size
+    );
+    // partition<KeyT, ValueT, CountT, BucketT, NUM_THREADS, THREAD_TILE, LOW_BIT, HIGH_BIT><<<grid_dim, block_dim, shmem_size>>>(key_array, size, workspace, out, debug_out);
     CUDA_CHECK(cudaDeviceSynchronize());
 
     uint32_t* debug_host = new uint32_t[NUM_BUCKETS];
