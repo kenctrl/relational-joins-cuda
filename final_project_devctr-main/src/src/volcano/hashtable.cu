0c5ed39cf5ca3139dd318d85993e0fd30563b56d
Siddhant Mukherjee <sidm1811@users.noreply.github.com>
2024-12-07 20:14:22 -0500

    Static memory for hashtable

--- final_project_devctr-main/src/src/volcano/hashtable.cu
+++ final_project_devctr-main/src/src/volcano/hashtable.cu
@@ -4,15 +4,14 @@
 #include <cstdint>
 
 // Hash table implementation
-template <typename KeyT, typename ValueT>
+template <typename KeyT, typename ValueT, int size>
 class HashTable {
 
     using IdxT = uint64_t;
     using ErrT = int;
 
-    KeyT *key_array;
-    ValueT *value_array;
-    int size;
+    KeyT key_array[size];
+    ValueT value_array[size];
 
 public:
     IdxT NOT_FOUND = (IdxT) -1;
@@ -22,28 +21,11 @@ public:
     ErrT SUCCESS = 0;
     ErrT FAILURE = 1;
 
-    __device__ HashTable() {
-        size = 0;
-        key_array = nullptr;
-        value_array = nullptr;
-    }
-
-    __device__ HashTable(int size) {
-        this->size = size;
-        key_array = new KeyT[size];
-        value_array = new ValueT[size];
-    }
-
-    __device__ ~HashTable() {
-        delete[] key_array;
-        delete[] value_array;
-    }
-
     size_t memory_usage() {
         return sizeof(KeyT) * size + sizeof(ValueT) * size + 1;
     }
 
-    __device__ IdxT hash(KeyT key) {
+    __device__ __forceinline__ IdxT hash(KeyT key) {
         IdxT x = (IdxT) key;
         x = ((x >> 16) ^ x) * 0x45d9f3b;
         x = ((x >> 16) ^ x) * 0x45d9f3b;
@@ -52,7 +34,7 @@ public:
     }
 
     // Hash function + linear probing
-    __device__ IdxT next_probe(KeyT key, int probe_idx) {
+    __device__ __forceinline__ IdxT next_probe(KeyT key, int probe_idx) {
         assert(probe_idx < size);
         return (probe_idx + hash(key)) % size;
     }
@@ -91,18 +73,19 @@ public:
 };;
 
 __global__ void hash_test(){
+    using HashTypeT = HashTable<int, int, 32>;
     assert(gridDim.x == 1 && gridDim.y == 1 && gridDim.z == 1);
     assert(blockDim.y == 1 && blockDim.z == 1);
 
     extern __shared__ char ht_memory[];
-    HashTable<int, int>* ht = reinterpret_cast<HashTable<int, int>*>(ht_memory);
+    HashTypeT* ht = reinterpret_cast<HashTypeT*>(ht_memory);
     if (threadIdx.x == 0) {
-        ht[0] = HashTable<int, int>(32);
+        ht[0] = HashTypeT();
     }
     __syncthreads();
     ht[0].insert(threadIdx.x, threadIdx.x);
     __syncthreads();
-    bool found = ht[0].find(threadIdx.x) != HashTable<int, int>().NOT_FOUND;
+    bool found = ht[0].find(threadIdx.x) != ht[0].NOT_FOUND;
     assert(found);
     assert(ht[0].get(threadIdx.x) == threadIdx.x);
 }
