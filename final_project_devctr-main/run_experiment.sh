c5ba708b8f2aa6380ba0cdba30d03aa847af46ea
kenctrl <kennethkchoi22@gmail.com>
2024-12-08 20:23:16 -0500

    binary recompilation easy

--- final_project_devctr-main/run_experiment.sh
+++ final_project_devctr-main/run_experiment.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Parse command line arguments
+run_initial=false
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -c|--compile) run_initial=true ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Run initial run to compile all executables only if flag is set
+if [ "$run_initial" = true ]; then
+    echo "Running initial run to compile all executables"
+    chmod +x initial_run.sh
+    ./initial_run.sh
+fi
+
+# Copy over the template build script into src/
+cp build-template.sh src/build.sh
+
 # Run process_yamls.sh and capture the number of YAMLs processed
 yaml_count=$(./process_yamls.sh)
 
